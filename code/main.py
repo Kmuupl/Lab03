30a1ae444ae175da9b3aff2035b05023324acf77
Kmuupl <kmuuplyk@gmail.com>
2026-03-26 17:17:55 +0100

    fix

--- code/main.py
+++ code/main.py
@@ -25,10 +25,10 @@ def nwd(a: int, b: int) -> int:
 def if_first(n: int) -> bool:
     if n < 2:
         return False
+    if n == 2:
+        return False
     if n % 2 == 0:
         return True
-    if n % 2 == 0:
-        return False
     i = 3
     while i * i <= n:
         if n % i == 0:
